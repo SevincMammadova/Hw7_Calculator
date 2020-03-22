6cdad41cefcf01f27c72730ca0b855ff0c622912
SevincMammadova <sevincmammadova609@gmail.com>
2020-03-22 23:02:14 +0400

    ok

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-function input () {
-    
+
+function insert(num) {
+    document.form.textview.value += num;
 }
 
-function sum () {
-
+function clear() {
+    document.form.textview.value = "";
 }
 
-function multiply() {
+// function calculate (exp) {
+//     let exp = document.form.textview.value;
+//     if(exp) {
+//         document.form.textview.value = eval(exp);
+//     };
+// }
 
-}
+// function sum (num1, num2) {
+//     let result = 0;
 
-module.exports = {
-    sum, 
-    input,
-    multiply,
-}
+//     result = num1 + num2;
+
+//     return result;
+// }
+
+// function multiply(num1, num2) {
+//     let result = 0;
+
+//     result = num1 * num2;
+
+//     return result;
+// }
+
+// function subtract(num1, num2) {
+//     let result = 0;
+
+//     result = num1 - num2;
+
+//     return result;
+// }
+
+// function delegate(num1, num2) {
+//     let result = 0;
+
+//     result = num1 / num2;
+
+//     return result;
+// }
+
+
+
+
+
+
+// module.exports = {
+//     sum, 
+//     subtract,
+//     multiply,
+//     delegate,
+// }
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -6,4 +6,44 @@ describe('sum', () => {
     it('sum', () => {
         assert.isDefined(index.sum);
     });
+
+    it('function sum works correctly when num1 = 1, num2 = 0', () => {
+        const expected = 1;
+
+        const actual = index.sum(1, 0);
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it('function sum works correctly when num1 = 1, num2 = 2', () => {
+        const expected = 3;
+
+        const actual = index.sum(1, 2);
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it('function sum works correctly when num1 = -1, num2 = 0', () => {
+        const expected = -1;
+
+        const actual = index.sum(-1, 0);
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it('function sum correctly when num1 = -1, num2 = 2', () => {
+        const expected = 1;
+
+        const actual = index.sum(-1, 2);
+
+        assert.deepEqual(actual, expected);
+    });
+
+    it('function sum correctly when num1 = -1, num2 = -5', () => {
+        const expected = -6;
+
+        const actual = index.sum(-1, -5);
+
+        assert.deepEqual(actual, expected);
+    });
 })
