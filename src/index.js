81a981ed7fd8a3097f0e42de50573708f1de9001
SevincMammadova <sevincmammadova609@gmail.com>
2020-03-26 01:55:15 +0400

    html and css ok

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
+let input = document.getElementById('input');
 
-function insert(num) {
-    document.form.textview.value += num;
+function insert (num) {
+   input.value += num;
 }
+ function clean() {
+     input.value = "";
+ }
 
-function clean() {
-    document.form.textview.value = "";
-}
+//  function sum (num1, num2) {
+//     i
+//  }
 
-function equal() {
-    let exp = document.form.textview.value;
+// function insert(num) {
+//     document.form.textview.value += num;
+// }
 
-    if (exp) {
-        document.form.textview.value = eval(exp);
-    }
-}
+// function clean() {
+//     document.form.textview.value = "";
+// }
 
-module.exports = {
-   clean,
-   equal,
-   insert
-}
+// function equal() {
+//     let exp = document.form.textview.value;
+
+//     if (exp) {
+//         document.form.textview.value = eval(exp);
+//     }
+// }
+
+// console.log(equal());
+
+// module.exports = {
+//    clean,
+//    equal,
+//    insert
+// }
+
+console.log(input());
